8f69801c7e334c2d9a4096d55dbf52d8655e045e
Adil-Ansar <ansari.adil2015@gmail.com>
2023-12-22 19:57:54 +0530

    Updated commonn functions

--- helper/comFun.js
+++ helper/comFun.js
@@ -1,10 +1,31 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 const jwtToken = async function (body) {
     const token = jwt.sign(body, process.env.JWT_KEY, { expiresIn: "1w" });
     return token;
 };
 
+const hashPassword = async (plainPassword) => {
+    try {
+        const hashedPassword = await bcrypt.hash(plainPassword, 10);
+        return hashedPassword;
+    } catch (error) {
+        throw new Error('Password hashing failed');
+    }
+};
+
+const comparePasswords = async (userProvidedPassword, storedHashedPassword) => {
+    try {
+        const result = await bcrypt.compare(userProvidedPassword, storedHashedPassword);
+        return result;
+    } catch (error) {
+        throw new Error('Password comparison failed');
+    }
+};
+
 module.exports = {
-    jwtToken
+    jwtToken,
+    hashPassword,
+    comparePasswords
 };
